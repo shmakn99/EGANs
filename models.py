9016355791c5b857f9fe5e9a289628f5d63d0098
koriavinash1 <koriavinash1@gmail.com>
2019-03-09 16:57:46 +0530

    VAE addition

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Organism(nn.Module):
   def __init__(self, gene_L, gene_D, num_hidden, len_support, dorpout = 0.5):
     super(Organism, self).__init__()
@@ -23,68 +24,151 @@ class Organism(nn.Module):
     
     return x
   
+#---------------------------------------------------------------
   
 class Cross_Over(nn.Module):
   def __init__(self, len_gene_L, len_gene_D, num_hidden, num_out, dropout = 0.5):
-      super(Gene2Phene, self).__init__()
-      
-      self.reproduce_L = nn,Sequential(
-          nn.Linear(2*len_gene_L, num_hidden),
-          nn.ReLU(),
-          nn.Dropout(dropout),
-          nn.Linear(num_hidden, num_out),
-          nn.Sigmoid(),
-      )
-      
-      self.reproduce_D = nn,Sequential(
-          nn.Linear(2*len_gene_D, num_hidden),
-          nn.ReLU(),
-          nn.Dropout(),
-          nn.Liner(num_hidden, num_out),
-          nn.Sigmoid(),
-      )
-      
+    super(Cross_Over, self).__init__()
+    
+    self.dropout = nn.Dropout(dropout)
+    self.relu = nn.ReLU()
+
+    #----------------------
+    self.L_fc1 = nn.Linear(2*len_gene_L, num_hidden)
+    self.L_fc21 = nn.Linear(num_hidden, num_out)
+    self.L_fc22 = nn.Linear(num_hidden, num_out)
+    
+    self.L_fc3 = nn.Linear(num_out, num_hidden)
+    self.L_fc4 = nn.Linear(num_hidden, len_gene_L)
+
+    #----------------------
+    
+    self.L_fc1 = nn.Linear(2*len_gene_L, num_hidden)
+    self.L_fc21 = nn.Linear(num_hidden, num_out)
+    self.L_fc22 = nn.Linear(num_hidden, num_out)
+    
+    self.L_fc3 = nn.Linear(num_out, num_hidden)
+    self.L_fc4 = nn.Linear(num_hidden, len_gene_L)
+    
+
+  def reparameterize(self, mu, logvar):
+
+    std = torch.exp(0.5*logvar)
+    eps = torch.randn_like(std)
+    return mu + eps*std
+
+
+  def encoder(self, support_L, support_D):
+
+    L_h1 = self.dropout(self.L_fc1_relu(support_L))
+    L = (self.L_fc21(L_h1), self.L_fc22(L_h1))
+
+    D_h1 = self.dropout(self.D_fc1_relu(support_D))
+    D = (self.D_fc21(D_h1), self.D_fc22(D_h1))
+
+    return L, D
+
+
+  def decoder(self, z_L, z_D):
+
+    L_h3 = self.dropout(self.relu(self.L_fc3(z_L)))
+    L = torch.sigmoid(self.fc4(L_h3))
+    
+    D_h3 = self.dropout(self.relu(self.D_fc3(z_D)))
+    D = torch.sigmoid(self.fc4(D_h3))
+
+    return L, D
+
+
   def forward(self, Par1, Par2):
     
     support_L = torch.cat(Par1,gene_L, Par_2.gene_L)
     support_D = torch.cat(Par1.gene_D, Par_2.gene_D)
     
-    offspring_L = reproduce(support_L)
-    offspring_D = reproduce(support_D)
+    mu_L, logvar_L = self.encoder(support_L)
+    z_L = self.reparameterize(mu_L, logvar_L)
+    offspring_L = self.decoder(z_L)
     
-    return offsping_L, offspring_D
+    mu_D, logvar_D = self.encoder(support_D)
+    z_D = self.reparameterize(mu_D, logvar_D)
+    offspring_D = self.decoder(z_D)
+
+    return offspring_L, offspring_D
   
+#---------------------------------------------------------------
   
 class Mutation(nn.Module):
   def __init__(self, len_gene_L, len_gene_D, num_hidden, num_out, dropout):
-    super(Gene2Phene, self).__init__()
+    super(Mutation, self).__init__()
     
-    self.mutate_L = nn.Sequential(
-        nn.Linear(len_gene_L, num_hidden),
-        nn.ReLU(),
-        nn.Dropout(dropout),
-        nn.Linear(num_hidden, num_out),
-        nn.Sigmoid()        
-    )
+    self.dropout = nn.Dropout(dropout)
+    self.relu = nn.ReLU()
+
+    #----------------------
+    self.L_fc1 = nn.Linear(len_gene_L, num_hidden)
+    self.L_fc21 = nn.Linear(num_hidden, num_out)
+    self.L_fc22 = nn.Linear(num_hidden, num_out)
     
-    self.mutate_D = nn.Sequential(
-        nn.Linear(len_gene_D, num_hidden),
-        nn.ReLU(),
-        nn.Dropout(dropout),
-        nn.Linear(num_hidden, num_out),
-        nn.Sigmoid()        
-    )
+    self.L_fc3 = nn.Linear(num_out, num_hidden)
+    self.L_fc4 = nn.Linear(num_hidden, len_gene_L)
+
+    #----------------------
+    
+    self.L_fc1 = nn.Linear(len_gene_L, num_hidden)
+    self.L_fc21 = nn.Linear(num_hidden, num_out)
+    self.L_fc22 = nn.Linear(num_hidden, num_out)
+    
+    self.L_fc3 = nn.Linear(num_out, num_hidden)
+    self.L_fc4 = nn.Linear(num_hidden, len_gene_L)
+
+  def reparameterize(self, mu, logvar):
+
+    std = torch.exp(0.5*logvar)
+    eps = torch.randn_like(std)
+    return mu + eps*std
+
+
+  def encoder(self, gene_L, gene_D):
+
+    L_h1 = self.dropout(self.L_fc1_relu(support_L))
+    L = (self.L_fc21(L_h1), self.L_fc22(L_h1))
+
+    D_h1 = self.dropout(self.D_fc1_relu(support_D))
+    D = (self.D_fc21(D_h1), self.D_fc22(D_h1))
+
+    return L, D
+
+
+  def decoder(self, z_L, z_D):
+
+    L_h3 = self.dropout(self.relu(self.L_fc3(z_L)))
+    L = torch.sigmoid(self.fc4(L_h3))
+    
+    D_h3 = self.dropout(self.relu(self.D_fc3(z_D)))
+    D = torch.sigmoid(self.fc4(D_h3))
+
+    return L, D
+
+
   def forward(self, Org):
-    mutated_L = mutate_L(Org.gene_L)
-    mutated_D = mutate_D(Org,gene_D)
+    
+    mu_L, logvar_L = self.encoder(Org.gene_L)
+    z_L = self.reparameterize(mu_L, logvar_L)
+    mutate_L = self.decoder(z_L)
+    
+    mu_D, logvar_D = self.encoder(org.gene_D)
+    z_D = self.reparameterize(mu_D, logvar_D)
+    mutated_D = self.decoder(z_D)
   
     return mutated_L, mutated_D
 
 
+#---------------------------------------------------------------
 
 class Fitness(nn.Module):
   def __init__(self, len_support, num_hidden,  dropout):
-    
+    super(Fitness, self).__init__()
+
     self.Gene2Phene = nn.Sequential(
         nn.Linear(len_support, num_hidden),
         nn.ReLU(),
